7887d9694aef23e4a0165b338726e6a9690a709a
Andrew Sapperstein <asapperstein@google.com>
2014-01-06 16:49:07 -0800

    Fix b/12348405. Return null drawable.
    The initial implementation of getDrawable always
    returned a drawable. However, the PhotoViewFragment
    used the null state for various state decisions.
    
    This made images that weren't on device in Gmail not
    show up and no downloading progress was shown. Now this
    happens. Hooray.
    
    Change-Id: I55df738f55353d9c4b599c2e497ca8ed4768c059

--- src/com/android/ex/photo/loaders/PhotoBitmapLoaderInterface.java
+++ src/com/android/ex/photo/loaders/PhotoBitmapLoaderInterface.java
@@ -19,19 +19,28 @@ public interface PhotoBitmapLoaderInterface {
         public Bitmap bitmap;
         public int status;
 
+        /**
+         * Returns a drawable to be used in the {@link com.android.ex.photo.views.PhotoView}.
+         * Should return null if the drawable is not ready to be shown (for instance, if
+         * the underlying bitmap is null).
+         */
         public Drawable getDrawable(Resources resources) {
             if (resources == null) {
                 throw new IllegalArgumentException("resources can not be null!");
             }
 
-            Drawable drawableToReturn;
             if (drawable != null) {
-                drawableToReturn = drawable;
-            } else {
-                drawableToReturn = new BitmapDrawable(resources, bitmap);
+                return drawable;
             }
 
-            return drawableToReturn;
+            // Don't create a new drawable if there's no bitmap. PhotoViewFragment regards
+            // a null drawable as a signal to keep showing the loading stuff.
+            // b/12348405.
+            if (bitmap == null) {
+                return null;
+            }
+
+            return new BitmapDrawable(resources, bitmap);
         }
     }
 }
